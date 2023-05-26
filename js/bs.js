0c1699686129361ffb5cf276537249f9f5f00946
dontexit <edwinhamal@gmail.com>
2023-05-26 15:05:24 +0545

    bro thought he knew bs but now he does

--- js/bs.js
+++ js/bs.js
@@ -1,29 +1,31 @@
-function bs(li,tar){
-    let start=0
-    let end=li.length-1
-    let m=Math.floor(start+end/2)
-    count=0
-    while (true && count<10){
-        console.log(start,end,m,li[m])
-        if(li[m]==tar){
-            console.log('true')
-            return true
+function bs1(nums, target) {
+    let s=0
+    let e=l
+    let m=Math.floor(s+e/2)
+    while (true){
+        console.log(s,e,m,nums[m])
+        if(m<0){
+            return -1
         }
-        if(li[m]>tar){
-            start=0
-            end=m-1
-            m=Math.floor(start+end/2)
-        }else{
-            start=m+1
-            m=Math.floor((start+li.length-1)/2)     
+        if(s==e){
+            if(nums[s]==target){ 
+                return nums[s]
+            }
+            return -1
         }
-        if(start==end){
-            console.log(li[end])
-            return
+        if(nums[m]==target){
+            return nums[m]
         }
-        count++
-
+       if(nums[m]>target){ 
+            e=m-1
+            m=Math.floor((s+e)/2)
+       }else{
+            s=m+1
+            m=Math.floor((s+e)/2) 
+       }
+        
     }
-}
+    
+};
 
-bs([1,2,3,4,5,6,8],9) 
+bs1([2,5],0)
